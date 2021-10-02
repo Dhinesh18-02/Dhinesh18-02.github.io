17952799888dc4d8dcbd1d8c778f967887344257
Mac <mac@Dhineshs-MBP.domain.name>
2021-10-02 23:56:03 +0530

    First commit

---
+++ precache-manifest.e6b849901e92ec829d4c78b2af1b872c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51895bf8ee585d534234b05cfd2afbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2b3e6c5db4b229745cc",
+    "url": "/static/css/main.d3fc2c0d.chunk.css"
+  },
+  {
+    "revision": "55a40a7db6e43fa52d07",
+    "url": "/static/js/2.ef5163b7.chunk.js"
+  },
+  {
+    "revision": "fb8e993bf740f49439948eabe43f0e99",
+    "url": "/static/js/2.ef5163b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b3e6c5db4b229745cc",
+    "url": "/static/js/main.df66a6e8.chunk.js"
+  },
+  {
+    "revision": "f438879c68bfc8864397",
+    "url": "/static/js/runtime-main.cc36b19e.js"
+  }
+]);
